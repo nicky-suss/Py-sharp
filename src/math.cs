d8d6b500a1a59f375e448586b63c1e48a3723a6c
nicky_sus <gfmm2558@gmail.com>
2026-04-16 21:13:59 +0300

    Add factorial()

--- src/math.cs
+++ src/math.cs
@@ -18,6 +18,13 @@ class math
     {
         return Math.Floor(x);
     }
+    public static long factorial(int x)
+    {
+        long res = 1;
+        for (int i = 1; i <= x; i++)
+            res *= i;
+        return res;
+    }
     public const double pi = Math.PI;
     public const double e = Math.E;
 }

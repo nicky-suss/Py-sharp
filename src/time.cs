c6e76ae2176608a5ee0fa944cbd1a09470627580
nicky_sus <gfmm2558@gmail.com>
2026-04-18 21:40:53 +0300

    Add time method

--- src/time.cs
+++ src/time.cs
@@ -12,4 +12,9 @@ public static class time
     /// </summary>
     /// <param name="seconds">The number of seconds to sleep.</param>
     public static void sleep(double seconds) => System.Threading.Thread.Sleep(Convert.ToInt32(seconds * 1000));
+    /// <summary>
+    /// Returns the current time in seconds since the Epoch (January 1, 1970, 00:00:00 UTC).
+    /// </summary>
+    /// <returns>Current time in seconds.</returns>
+    public static double Time() => System.DateTimeOffset.UtcNow.ToUnixTimeSeconds();
 }

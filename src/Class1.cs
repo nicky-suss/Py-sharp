7d3660cf33467cf18f85eea9393ef383e0506885
nicky_sus <gfmm2558@gmail.com>
2026-04-18 09:35:52 +0300

    Add xml hints

--- src/Class1.cs
+++ src/Class1.cs
@@ -2,10 +2,19 @@
 
 public static class Base
 {
+    /// <summary>
+    /// Prints an object to the text stream, followed by a line terminator
+    /// </summary>
+    /// <param name="obj">The object to print</param>
     public static void print(object? obj = null)
     {
         System.Console.WriteLine(obj ?? "");
     }
+    /// <summary>
+    /// Displays a prompt and reads a line of characters from the standard input stream.
+    /// </summary>
+    /// <param name="obj">The message to display before reading input.</param>
+    /// <returns>The line of characters read from the input.</returns>
     public static string input(string? obj = null)
     {
         if (obj != null)
@@ -14,26 +23,51 @@ public static class Base
         }
         return System.Console.ReadLine() ?? "";
     }
+    /// <summary>
+    /// Converts a value to an integer.
+    /// </summary>
     public static int Int(object obj)
     {
         return (int)Convert.ToInt32(obj);
     }
+    /// <summary>
+    /// Converts a value to a single-precision floating-point number.
+    /// </summary>
+    /// <param name="obj">The value to convert.</param>
+    /// <returns>A floating-point number equivalent to the input.</returns>
     public static float Float(object obj)
     {
         return Convert.ToSingle(obj);
     }
+    /// <summary>
+    /// Converts a value to its string representation.
+    /// </summary>
+    /// <param name="obj">The value to convert.</param>
+    /// <returns>The string representation of the object.</returns>
     public static string str(object obj)
     {
         return Convert.ToString(obj);
     }
+    /// <summary>
+    /// Returns the length (the number of items) of an object.
+    /// </summary>
     public static int len(dynamic obj)
     {
         if (obj is System.Array a) return a.Length;
         if (obj is string s) return s.Length;
         return obj.Count;
     }
+    /// <summary>
+    /// Terminates the current process and returns an exit code to the operating system.
+    /// </summary>
     public static void exit() => System.Environment.Exit(0);
 
+    /// <summary>
+    /// Represents the boolean value True.
+    /// </summary>
     public const bool True = true;
+    /// <summary>
+    /// Represents the boolean value False.
+    /// </summary>
     public const bool False = false;
 }
--- src/math.cs
+++ src/math.cs
@@ -2,10 +2,27 @@ namespace PySharp;
 
 class math
 {
+    /// <summary>
+    /// Returns the square root of a specified number.
+    /// </summary>
     public static double sqrt(double x) => Math.Sqrt(x);
+    /// <summary>
+    /// Returns a specified number raised to the specified power.
+    /// </summary>
     public static double pow(int x, int y) => Math.Pow(x, y);
+    /// <summary>
+    /// Returns the smallest integral value that is greater than or equal to the specified number.
+    /// </summary>
+    /// <param name="x">A double-precision floating-point number.</param>
     public static double ceil(double x) => Math.Ceiling(x);
+    /// <summary>
+    /// Returns the largest integral value that is less than or equal to the specified number.
+    /// </summary>
+    /// <param name="x">A double-precision floating-point number.</param>
     public static double floor(double x) => Math.Floor(x);
+    /// <summary>
+    /// Returns the factorial of a number.
+    /// </summary>
     public static long factorial(int x)
     {
         long res = 1;
@@ -13,11 +30,32 @@ class math
             res *= i;
         return res;
     }
+    /// <summary>
+    /// Converts angle x from degrees to radians.
+    /// </summary>
     public static double radians(double degrees) => degrees * Math.PI / 180;
+    /// <summary>
+    /// Converts angle x from radians to degrees.
+    /// </summary>
     public static double degrees(double radians) => radians * 180 / Math.PI;
+    /// <summary>
+    /// Returns the sine of the specified angle (in radians).
+    /// </summary>
     public static double sin(double x) => Math.Sin(x);
+    /// <summary>
+    /// Returns the cosine of the specified angle (in radians).
+    /// </summary>
     public static double cos(double x) => Math.Cos(x);
+    /// <summary>
+    /// Returns the tangent of the specified angle (in radians).
+    /// </summary>
     public static double tan(double x) => Math.Tan(x);
+    /// <summary>
+    /// The mathematical constant PI.
+    /// </summary>
     public const double pi = Math.PI;
+    /// <summary>
+    /// The mathematical constant E.
+    /// </summary>
     public const double e = Math.E;
 }
